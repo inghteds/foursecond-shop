b450e98711903d994e99e5bb8e042ec41f04975f
inghteds <yuto_092424@icloud.com>
2025-11-03 12:23:42 +0900

    seven commit

--- app/m-423154667/layout.tsx
+++ app/m-423154667/layout.tsx
@@ -1,6 +1,10 @@
 export const metadata = {
   robots: {
-    index: false,  // ← 検索結果に出さない
+    index: false, // 検索結果に出さない
     follow: false,
   },
 };
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
